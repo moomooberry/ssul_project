6db507b3f9b7de169088d4bd69c072b32a08e0dd
ghh237 <ghh237@naver.com>
2023-08-08 16:58:37 +0900

    추가: 컴포넌트 (HashtagBadge)

---
+++ src/components/badge/HashtagBadge.tsx
@@ -0,0 +1,37 @@
+import { FC, MouseEventHandler } from "react";
+import styled from "styled-components";
+import CloseIcon from "../icons/CloseIcon";
+
+const Badge = styled.div`
+  position: relative;
+  padding: 5px;
+  background-color: #e8e8e8;
+  color: #717171;
+  border-radius: 8px;
+  min-width: fit-content;
+`;
+const IconWrapper = styled.div`
+  position: absolute;
+  top: -8px;
+  right: -16px;
+  z-index: 2;
+  cursor: pointer;
+`;
+
+interface HashtagBadgeProps {
+  text: string;
+  onDeleteClick?: MouseEventHandler<HTMLDivElement>;
+}
+
+const HashtagBadge: FC<HashtagBadgeProps> = ({ text, onDeleteClick }) => (
+  <Badge>
+    {text}
+    {onDeleteClick && (
+      <IconWrapper onClick={onDeleteClick}>
+        <CloseIcon />
+      </IconWrapper>
+    )}
+  </Badge>
+);
+
+export default HashtagBadge;
