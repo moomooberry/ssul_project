9e46249dac63af9da19c52296df9874d86a4abca
ghh237 <ghh237@naver.com>
2023-08-15 02:05:37 +0900

    수정: 메인 페이지 (4)
    server 데이터 get

--- src/screens/home/HomeView.tsx
+++ src/screens/home/HomeView.tsx
@@ -2,14 +2,17 @@ import { FC } from "react";
 import UI from "./components/styled";
 import dayjs from "dayjs";
 import SsulCard from "@/components/card/SsulCard";
-import TestImgSrc from "@/test/images/testImageLong.jpg";
 import AddCard from "@/components/card/AddCard";
+import { GetPostListResponse } from "@/types/api/post/getPostList";
+import SkeletonCard from "@/components/card/SkeletonCard";
 
 export interface HomeViewProps {
   isAdmin: boolean;
+  isLoading: boolean;
+  data?: GetPostListResponse;
 }
 
-const HomeView: FC<HomeViewProps> = ({ isAdmin }) => (
+const HomeView: FC<HomeViewProps> = ({ isAdmin, isLoading, data }) => (
   <UI.Layout>
     <UI.Container>
       <UI.Title>
@@ -18,50 +21,30 @@ const HomeView: FC<HomeViewProps> = ({ isAdmin }) => (
       </UI.Title>
       <UI.CardWrapper>
         {isAdmin && <AddCard />}
-        <SsulCard
-          id={0}
-          date=""
-          views={0}
-          title=""
-          imgSrc=""
-          link=""
-          hashtags={undefined}
-          isLoading={true}
-          isAdmin={isAdmin}
-        />
-        <SsulCard
-          id={1}
-          date={dayjs().format("YYYY.MM.DD")}
-          views={0}
-          title="제목 입니다2"
-          imgSrc={undefined}
-          link="https://www.naver.com"
-          hashtags={undefined}
-          isLoading={false}
-          isAdmin={isAdmin}
-        />
-        <SsulCard
-          id={2}
-          date={dayjs().format("YYYY.MM.DD")}
-          views={9999}
-          title="제목 입니다3"
-          imgSrc={TestImgSrc}
-          link="https://www.naver.com"
-          isLoading={false}
-          hashtags={["#안녕", "#하세요"]}
-          isAdmin={isAdmin}
-        />
-        <SsulCard
-          id={3}
-          date={dayjs().format("YYYY.MM.DD")}
-          views={9999}
-          title="길어져버린 제목 입니다길어져버린 제목 입니다. 길어져버린 제목 입니다 길어져버린 제목 입니다"
-          imgSrc={TestImgSrc}
-          link="https://www.naver.com"
-          hashtags={["#길어버린해시태그", "#테스트", "#해시태그", "#입니다"]}
-          isLoading={false}
-          isAdmin={isAdmin}
-        />
+        {isLoading && (
+          <>
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+          </>
+        )}
+        {!isLoading &&
+          data?.map((item) => (
+            <SsulCard
+              key={item.id}
+              id={item.id}
+              date={dayjs(item.created).format("YYYY.MM.DD")}
+              views={item.views}
+              title={item.title}
+              imgSrc={item.imgSrc}
+              link={item.link}
+              hashtags={item.hashtags}
+              isAdmin={isAdmin}
+            />
+          ))}
       </UI.CardWrapper>
     </UI.Container>
   </UI.Layout>
