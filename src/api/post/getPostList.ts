d1aac64bea1a9d5b6355266ecb5366ab4b9a9c60
ghh237 <ghh237@naver.com>
2023-08-22 16:45:13 +0900

    수정: api (getPostList, upPostView) response 타입 적용

--- src/api/post/getPostList.ts
+++ src/api/post/getPostList.ts
@@ -1,9 +1,13 @@
+import { ApiResponse } from "@/types/api";
 import api from "..";
 import { GetPostListResponse } from "@/types/api/post/getPostList";
 
 const getPostList = async () => {
-  const { data } = await api.get<GetPostListResponse>("/post");
-  return data;
+  const {
+    data: { result },
+  } = await api.get<ApiResponse<GetPostListResponse>>("/post");
+
+  return result;
 };
 
 export default getPostList;
--- src/api/post/upPostView.ts
+++ src/api/post/upPostView.ts
@@ -1,13 +1,17 @@
 import { UpPostViewResponse } from "@/types/api/post/upPostview";
 import api from "..";
+import { ApiResponse } from "@/types/api";
 
 interface UpPostViewProps {
   id: string;
 }
 
 const upPostView = async ({ id }: UpPostViewProps) => {
-  const { data } = await api.post<UpPostViewResponse>("/post/view-up", { id });
-  return data;
+  const {
+    data: { result },
+  } = await api.post<ApiResponse<UpPostViewResponse>>("/post/view-up", { id });
+
+  return result;
 };
 
 export default upPostView;
