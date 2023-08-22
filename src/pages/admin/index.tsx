bf208416c3a0d9338c5c7b9f5a5d54d2641763d9
ghh237 <ghh237@naver.com>
2023-08-22 16:58:03 +0900

    수정: 메인 페이지 (3)
    admin으로 들어갈시 verification으로 refreshToken의 유효성 검사를 진행

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,6 +1,24 @@
+import verification from "@/api/auth/verification";
 import HomeController from "@/screens/home/HomeController";
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 
 const AdminPage: NextPage = () => <HomeController isAdmin={true} />;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  try {
+    const refreshToken = req.cookies["you_and_my_story_refresh_token"];
+    await verification({ refreshToken });
+    return {
+      props: {},
+    };
+  } catch (e) {
+    return {
+      redirect: {
+        destination: "/auth/login",
+        permanent: true,
+      },
+    };
+  }
+};
 
 export default AdminPage;
