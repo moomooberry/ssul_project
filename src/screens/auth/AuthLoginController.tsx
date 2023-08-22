f0ab07e49ed7e2d1d79ef86bbf01797363b2006e
ghh237 <ghh237@naver.com>
2023-08-22 16:50:29 +0900

    수정: 로그인 페이지 (2)
    1. 로그인 api 적용

--- src/screens/auth/AuthLoginController.tsx
+++ src/screens/auth/AuthLoginController.tsx
@@ -1,8 +1,55 @@
-import { FC } from "react";
-import AuthLoginView, { AuthLoginViewProps } from "./AuthLoginView";
+import { FC, useCallback } from "react";
+import AuthLoginView, {
+  AuthLoginFormFields,
+  AuthLoginViewProps,
+} from "./AuthLoginView";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import login from "@/api/auth/login";
+import { useRouter } from "next/router";
+import { AxiosError, AxiosResponse } from "axios";
+import useAppDispatch from "@/hooks/useAppDispatch";
+import { setToken } from "@/store/modules/auth";
+import { LoginResponse } from "@/types/api/auth/login";
+import { ApiResponse } from "@/types/api";
 
 const AuthLoginController: FC = () => {
-  const viewProps: AuthLoginViewProps = {};
+  const router = useRouter();
+
+  const dispatch = useAppDispatch();
+
+  const loginMutation = useMutation({
+    mutationFn: login,
+    onSuccess: (accessToken) => {
+      dispatch(setToken(accessToken));
+      router.push("/admin");
+    },
+    onError: (err: AxiosError<ApiResponse<LoginResponse>>) => {
+      if (err.isAxiosError) {
+        console.log("err", err);
+        alert(`${err.response?.data.result}`);
+      } else {
+        alert("unknown error입니다 ❌");
+      }
+    },
+  });
+
+  const { handleSubmit, register } = useForm<AuthLoginFormFields>();
+
+  const onValid = useCallback<SubmitHandler<AuthLoginFormFields>>(
+    ({ id, password }) => {
+      loginMutation.mutate({ id, password });
+    },
+    [loginMutation]
+  );
+
+  const viewProps: AuthLoginViewProps = {
+    register: {
+      id: register("id"),
+      password: register("password"),
+    },
+    onSubmitClick: handleSubmit(onValid),
+  };
 
   return <AuthLoginView {...viewProps} />;
 };
--- src/screens/auth/AuthLoginView.tsx
+++ src/screens/auth/AuthLoginView.tsx
@@ -1,22 +1,37 @@
-import { FC } from "react";
+import { FC, MouseEventHandler } from "react";
 import UI from "./components/styled";
 import Button from "@/components/button";
+import { UseFormRegisterReturn } from "react-hook-form";
 
-export interface AuthLoginViewProps {}
+export interface AuthLoginFormFields {
+  id: string;
+  password: string;
+}
 
-const AuthLoginView: FC<AuthLoginViewProps> = ({}) => (
+export interface AuthLoginViewProps {
+  register: {
+    [K in keyof AuthLoginFormFields]: UseFormRegisterReturn;
+  };
+  onSubmitClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+const AuthLoginView: FC<AuthLoginViewProps> = ({ register, onSubmitClick }) => (
   <UI.Layout>
     <UI.Container>
       <UI.StyledForm>
         <UI.StyledLabel>
           <UI.StyledSpan>아이디를 입력하세요</UI.StyledSpan>
-          <UI.StyledInput autoComplete="username" />
+          <UI.StyledInput autoComplete="username" {...register.id} />
         </UI.StyledLabel>
         <UI.StyledLabel>
           <UI.StyledSpan>비밀번호를 입력하세요</UI.StyledSpan>
-          <UI.StyledInput type="password" autoComplete="current-password" />
+          <UI.StyledInput
+            type="password"
+            autoComplete="current-password"
+            {...register.password}
+          />
         </UI.StyledLabel>
-        <Button text="로그인" />
+        <Button text="로그인" onClick={onSubmitClick} />
       </UI.StyledForm>
     </UI.Container>
   </UI.Layout>
