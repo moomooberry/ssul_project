6248d4c3fcf6be2d227fec5f8ac9693bf433adc2
ghh237 <ghh237@naver.com>
2023-08-28 17:02:11 +0900

    수정: api (getPostList) pagination 처리

--- src/api/post/getPostList.ts
+++ src/api/post/getPostList.ts
@@ -1,11 +1,25 @@
-import { ApiResponse } from "@/types/api";
+import {
+  ApiResponse,
+  PaginationRequest,
+  PaginationResponse,
+} from "@/types/api";
 import api from "..";
 import { GetPostListResponse } from "@/types/api/post/getPostList";
 
-const getPostList = async () => {
+interface GetPostListProps extends PaginationRequest {}
+
+const getPostList = async ({ p = 1, ps = 20 }: GetPostListProps) => {
   const {
     data: { result },
-  } = await api.get<ApiResponse<GetPostListResponse>>("/post");
+  } = await api.get<ApiResponse<PaginationResponse<GetPostListResponse>>>(
+    "/post",
+    {
+      params: {
+        p,
+        ps,
+      },
+    }
+  );
 
   return result;
 };
--- src/types/api/index.ts
+++ src/types/api/index.ts
@@ -6,3 +6,15 @@ export interface ApiResponse<T> {
 export interface ApiRequestAccessToken {
   accessToken: string | null;
 }
+
+export interface PaginationResponse<K> {
+  results: K;
+  page: number;
+  totalPages: number;
+  totalResults: number;
+}
+
+export interface PaginationRequest {
+  p?: number;
+  ps?: number;
+}
