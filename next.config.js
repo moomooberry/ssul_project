a63e334fa88ce469e3768311ab68c11e73d2c55d
ghh237 <ghh237@naver.com>
2023-08-28 13:43:08 +0900

    수정: next.config.js (이미지 도메인 추가)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    domains: ["youandmystory-bucket.s3.ap-northeast-2.amazonaws.com"],
+  },
 };
 
 module.exports = nextConfig;
