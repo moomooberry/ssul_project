20848ee287eba8698dc1a35247090bdfa5f264fa
ghh237 <ghh237@naver.com>
2023-09-07 22:30:24 +0900

    수정: styled (title-header)
    pc 화면 sticky 할때 width 100vw 및 애니메이션 효과

--- src/screens/home/components/styled.ts
+++ src/screens/home/components/styled.ts
@@ -22,25 +22,27 @@ const Container = styled.div`
 `;
 
 const Title = styled.h1<{ $isSticky: boolean }>`
-  font-size: 36px;
   font-weight: 400;
   letter-spacing: -0.54px;
   box-sizing: border-box;
   margin-bottom: 20px;
   text-align: center;
   background-color: #ffffff;
-  width: 700px;
-  padding: 20px 40px;
-  border-radius: 15px;
+  width: ${({ $isSticky }) => ($isSticky ? "100vw" : "700px")};
+  padding: ${({ $isSticky }) => ($isSticky ? "16px 20px" : "20px 40px")};
+  border-radius: ${({ $isSticky }) => ($isSticky ? 0 : "15px")};
+  font-size: ${({ $isSticky }) => ($isSticky ? "24px" : "36px")};
+  transition: width 1s, padding 1s, border-radius 3s, font-size 1s;
+
+  position: sticky;
+  top: 0;
+  z-index: 2;
   box-shadow: 0 5px 20px 0 rgba(213, 213, 213, 0.47);
   @media (max-width: 1024px) {
     font-size: ${({ $isSticky }) => ($isSticky ? "20px" : "24px")};
     width: ${({ $isSticky }) => ($isSticky ? `calc(100% + 40px)` : `100%`)};
     padding: ${({ $isSticky }) => ($isSticky ? `15px 40px` : `30px 40px`)};
     border-radius: ${({ $isSticky }) => ($isSticky ? "0px" : "15px")};
-    position: sticky;
-    top: 0;
-    z-index: 2;
     transition: width 1s, padding 1s;
   }
 `;
